37dd13759f23d4f00798ee13a8eb25bd16b0f365
officialderick <derekfredrick02@gmail.com>
2024-05-03 19:19:32 +0300

    First Commit

---
+++ the code/Go.py
@@ -0,0 +1,76 @@
+from tkinter import *
+from tkinter import messagebox
+import base64
+
+screen = Tk()
+screen.geometry("400x400")
+screen.title("Encrypt and Decrypt")
+screen.configure(bg="grey")
+def encrypt():
+    password=code.get()
+    if password== "1234":
+        screen1=Toplevel(screen)
+        screen1.title("Encryption")
+        screen1.geometry("400x250")
+        screen1.configure(bg="light blue")
+
+        message=text1.get(1.0,END)
+        encode_message = message.encode("ascii")
+        base64_bytes = base64.b64encode(encode_message)
+        encrypt = base64_bytes.decode("ascii")
+
+        Label(screen1,text="Text is Encrypted",font="impack 10 bold").place(x=5, y=6)
+        text2 = Text(screen1,font="30",bd=4,wrap=WORD)
+        text2.place(x=2, y=30, width=390,height=180)
+        text2.insert(END,encrypt)
+
+    elif(password==""):
+        messagebox.showerror("Error","please enter the secret key")
+    elif(password!="1234"):
+        messagebox.showerror("Oops!!", "Invalid secret key")
+
+def decrypt():
+    password = code.get()
+    if password == "1234":
+        screen2 = Toplevel(screen)
+        screen2.title("Decryption")
+        screen2.geometry("400x250")
+        screen2.configure(bg="light green")
+
+        message = text1.get(1.0, END)
+        encode_message = message.encode("ascii")
+        base64_bytes = base64.b64decode(encode_message)
+        encrypt = base64_bytes.decode("ascii")
+
+        Label(screen2, text="Text is Decrypted", font="impack 10 bold").place(x=5, y=6)
+        text2 = Text(screen2, font="30", bd=4, wrap=WORD)
+        text2.place(x=2, y=30, width=390, height=180)
+        text2.insert(END, encrypt)
+
+    elif (password == ""):
+        messagebox.showerror("Error", "please enter the secret key")
+    elif (password != "1234"):
+        messagebox.showerror("Oops!!", "Invalid secret key")
+
+def reset():
+    text1.delete(1.0,END)
+    code.set("")
+
+
+#label
+Label(screen, text=" enter text for encryption and decryption ", font="impack 14 bold").place(x=5, y=6)
+#Text
+text1=Text(screen,font="20")
+text1.place(x=5,y=45,width=390,height=120)
+#Label
+Label(screen, text="Enter Secret Key", font="impack 13 bold").place(x=130, y=185)
+#entry
+code=StringVar()
+Entry(textvariable=code    , bd=4, font="20", show="*").place(x=110, y=220)
+#Button
+Button(screen, text="ENCRYPT", font="arial 15 bold", bg="red",fg="white", command=encrypt).place(x=15, y=280, width=180)
+Button(screen, text="DECRYPT", font="arial 15 bold", bg="green",fg="white", command=decrypt).place(x=220, y=280, width=180)
+Button(screen, text="RESET", font="arial 15 bold", bg="blue",fg="white", command=reset).place(x=60, y=350, width=280)
+mainloop()
+
+#This is a encryption and decryption tool made using base 64 by Derick Gitau
